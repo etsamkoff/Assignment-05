373d6eb042a038c79dc57e3c5d0d85b4f0aca719
etsamkoff <55714950+etsamkoff@users.noreply.github.com>
2019-11-21 11:18:13 -0500

    Update Assignment 05.R

--- Assignment 05.R
+++ Assignment 05.R
@@ -1,6 +1,6 @@
 if (!require("data.table")) install.packages("data.table")
 library("data.table")
 ptm <- proc.time()
-DF <- fread("GDP.csv", header="auto", 
+DF <- fread("A191RL1Q225SBEA.csv", header="auto", 
             data.table=FALSE)
 DF <- as.data.frame(DF)
